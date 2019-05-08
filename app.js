f302c9e19d6efe329ab51dc82793b540a5a61d38
Dustin VanCour <dustin82@gmail.com>
2019-05-08 16:39:01 -0600

    pausing for now

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var x = ?
-var compChoice = Math.random();
-if compChoice
+var compChoice = Math.floor(Math.random() * 3 + 1);
 
-function play() {
+function play(num) {
+  if compChoice
     //Need to determine variables that equal r-p-s
     //Need if-else staements to determine the winner
   }
